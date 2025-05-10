b2e9af7bc852e10f66bfa7dec4d48f95ca12e8d1
Alefo <alephsilva12@gmail.com>
2025-05-09 23:32:26 -0300

    v0.2 - fixes

--- inject.js
+++ inject.js
@@ -1,21 +1,17 @@
-chrome.storage.local.get(['fbcg'], function(result) {
-    if(result.fbcg.enabled == true){
-        console.log("Facebook Background Changer:  A extensao está ativada! (fbcg.enabled == true)");
-        applyBg(result.fbcg.img);
-    }
-    else {
-        console.log("Facebook Background Changer: Extensão desativada, portanto a imagem não será aplicada! (fbcg.enabled == false)")
-    }
-});
-function applyBg(x) {
-    try {
-        let imgElement = document.createElement("img");
-        imgElement.id = "fbcg_wall";
-        document.body.insertBefore(imgElement, document.body.children[0]);    
-        imgElement.src = x;
-    }
-    catch {
-        imgElement.src = x;
-    }
-}
 
+let imgElement;
+chrome.storage.local.get(r=>{
+    if(r.img){
+        fbcgApplyBackground(r.img);
+    }
+})
+
+function fbcgApplyBackground(img){
+    if(document.getElementById('fbcg') == undefined && document.getElementsByClassName('fbcg')[0] == undefined){
+        document.body.className+=' fbcg';
+        imgElement = document.createElement("img");
+        imgElement.id = "fbcg";
+        document.body.appendChild(imgElement); 
+    }
+    imgElement.src = img;
+}
